2f87367791aae37daf7b4f6be1a98adf7c0b76fa
Ayoola David <ayooladavid@gmail.com>
2020-06-14 16:55:24 +0100

    add edited files

---
+++ build/precache-manifest.23e1e52c072c50dc0e6fde60812fe08d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a88069c5dc23a7c8682ea121de59204",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a5f99c15f862cf1b95a",
+    "url": "/static/css/main.b068b2ff.chunk.css"
+  },
+  {
+    "revision": "856b519d8892e9d096a5",
+    "url": "/static/js/2.7ff664be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7ff664be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a5f99c15f862cf1b95a",
+    "url": "/static/js/main.c69750f0.chunk.js"
+  },
+  {
+    "revision": "c411e6e668540823748c",
+    "url": "/static/js/runtime-main.3ce3c05c.js"
+  }
+]);
