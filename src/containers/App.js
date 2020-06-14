d79ee2ca7dc346e6470b0898d9a85a6e446dfaa6
Ayoola David <ayooladavid@gmail.com>
2020-06-14 15:36:03 +0100

    add edited files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from 'react'
 import Header from '../components/Header/Header'
 import Profile from './Profile'
-import logo from '../logo.svg';
-import './App.css';
+import logo from '../logo.svg'
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`text-align: center;`
 
 function App() {
   return (
-    <div className="App">
+    <AppWrapper>
       <Header logo={logo}/>
       <Profile />
-    </div>
+    </AppWrapper>
   );
 }
 
-export default App;
+export default App
