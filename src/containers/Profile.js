ff27e95536db7cdcc3e5f30da0b3b09164620136
Ayoola David <ayooladavid@gmail.com>
2020-06-14 16:54:34 +0100

    add edited files

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -15,26 +15,41 @@ class Profile extends Component {
         super();
         this.state = {
             data: {},
+            repositories: [],
             loading: true,
+            error: '',
         }
     }
 
     async componentDidMount() {
+      try {
         const profile = await
         fetch('https://api.github.com/users/dqve');
         const profileJSON = await profile.json();
         if (profileJSON) {
+            const repositories = await fetch(profileJSON.repos_url);
+            const repositoriesJSON = await repositories.json();
+
             this.setState({
                 data: profileJSON,
+                repositories: repositoriesJSON,
                 loading: false,
             })
         }
+      }
+       catch(error) {
+        this.setState({
+            loading: false,
+            error: error.message,
+        });
+      }
     }
 
     render() {
-       const { data, loading } = this.state;
-        if (loading) {
-        return <div>Loading...</div>;
+        const { data, loading, repositories, error } = this.state
+
+        if (loading || error) {
+            return <div>{loading ? 'Loading...' : error}</div>
         }
 
          const items = [
@@ -46,11 +61,18 @@ class Profile extends Component {
             { label: 'email', value: data.email },
             { label: 'bio', value: data.bio }
         ]
+        
+        const projects = repositories.map(repository => ({
+            label: repository.name,
+            value: <Link url={repository.html_url} title='Github URL' />
+        }));
+
 
         return (
         <ProfileWrapper>
             <Avatar className='Profile-avatar' src={data.avatar_url} alt='avatar' />
-                <List items={items} />
+                <List title='Profile' items={items} />
+                <List title='Projects' items={projects} />
         </ProfileWrapper>
         );
     }   
